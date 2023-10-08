bf5b857a8e70437adc85ba8a1d5c1656bcf16f68
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-08 23:14:01 +0900

    13549

--- 17951.py
+++
@@ -1,8 +0,0 @@
-n, k = map(int, input().split())
-data = list(map(int, input().split()))
-
-sorted(data)
-group = [0] * k
-while data:
-    for j in range(k):
-        group[j]
--- ps.py
+++ ps.py
@@ -1,39 +1,34 @@
+# 13549
+
 import sys
 from collections import deque
 
 def bfs(start, end):
     if start == end:
-        return (0, 1)
+        return 0
     q = deque([start])
     arr = [-1] * (100001)
     arr[start] = 0
-    rst = 0
-    count = 0
     while q:
         x = q.popleft()
     
         for i in range(3):
             if i == 0:
                 nx = x-1
+                tmp = 1
             elif i == 1:
                 nx = x+1
+                tmp = 1
             else:
                 nx = x*2
+                tmp = 0
             if 0<= nx <= 100000:
-                if (nx == end):
-                    if rst == 0:
-                        rst = arr[x] + 1
-                        count += 1
-                    elif arr[x]+1 == rst:
-                        count += 1
-                if (arr[nx] == -1 or arr[nx] == arr[x] + 1) and count == 0:
+                if arr[nx] == -1 or arr[x]+tmp < arr[nx]:
+                    arr[nx] = arr[x]+tmp
                     q.append(nx)
-                    arr[nx] = arr[x]+1
-
-    return (rst, count)
+    return arr[end]
 
 input = sys.stdin.readline
 start, end = map(int, input().split())
 num = bfs(start, end)
-for r in num:
-    print(r)
+print(num)
