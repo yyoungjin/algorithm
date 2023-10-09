6e616d252573e071e68bea381e80f81a54dd1d53
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-09 22:14:08 +0900

    13913

--- ps.py
+++ ps.py
@@ -1,34 +1,29 @@
-# 13549
+# 13913
 
 import sys
 from collections import deque
 
 def bfs(start, end):
-    if start == end:
-        return 0
     q = deque([start])
-    arr = [-1] * (100001)
-    arr[start] = 0
+    arr = [0] * 100001
+    path = [0] * 100001
     while q:
         x = q.popleft()
-    
-        for i in range(3):
-            if i == 0:
-                nx = x-1
-                tmp = 1
-            elif i == 1:
-                nx = x+1
-                tmp = 1
-            else:
-                nx = x*2
-                tmp = 0
-            if 0<= nx <= 100000:
-                if arr[nx] == -1 or arr[x]+tmp < arr[nx]:
-                    arr[nx] = arr[x]+tmp
-                    q.append(nx)
-    return arr[end]
+        if x == end:
+            print(arr[x])
+            res = str(x)
+            a = x
+            for _ in range(arr[x]):
+                a = path[a]
+                res = str(a) + ' ' + res
+            print(res)
+            return
+        for nx in (x-1, x+1, 2*x):
+            if 0<= nx <= 100000 and arr[nx] == 0:
+                q.append(nx)
+                arr[nx] = arr[x] + 1
+                path[nx] = x # x로부터 왔다는 것을 기록
 
 input = sys.stdin.readline
 start, end = map(int, input().split())
-num = bfs(start, end)
-print(num)
+bfs(start, end)
