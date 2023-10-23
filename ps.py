d0c508aae75f558e6bb1abe9bcea2c36332b257b
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-23 17:52:21 +0900

    3665

--- ps.py
+++ ps.py
@@ -1,31 +1,55 @@
 # 위상 정렬
+import sys
+input = sys.stdin.readline
 from collections import deque
-n, m = map(int, input().split())
-table = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
-arr = [0] * (n+1)
-for _ in range(m):
-    a, b = map(int, input().split())
-    table[a].append(b) # 다음 갈 곳
-    arr[b] += 1 # 내가 실행되는 조건의 수
+t = int(input())
 
-q = deque([])
-res = []
-# 시작점 찾기
-for i in range(1, n+1):
-    if not arr[i]:
-        q.append(i)
-        visited[i] = True
+def test():
+    n = int(input())
+    table = [[] for _ in range(n+1)]
+    arr = [0] * (n+1)
+    change = set()
+    past_rank = [0] + list(map(int, input().split()))
+    m = int(input())
+    for _ in range(m):
+        a, b = map(int, input().split())
+        change.add((a, b))
+    
+    for i in range(1, n+1):
+        for j in range(i+1, n+1):
+            a = past_rank[i]
+            b = past_rank[j]
+            if (b, a) in change or (a, b) in change:
+                a, b = b, a
+            table[a].append(b)
+            arr[b] += 1
 
-while q:
-    node = q.popleft()
-    res.append(str(node)) # 결과에 기록
-    # 노드 제거
-    for i in table[node]:
-        arr[i] -= 1
-        if arr[i] == 0:
-            if not visited[i]:
+    q = deque([])
+    res = []
+    # 시작점 찾기
+    for i in range(1, n+1):
+        if not arr[i]:
+            q.append(i)
+            arr[i] -= 1 # 방문처리
+
+    while q:
+        node = q.popleft()
+        res.append(str(node)) # 결과에 기록
+        # 노드 제거
+        for i in table[node]:
+            arr[i] -= 1
+            if arr[i] == 0:
                 q.append(i)
-                visited[i] = True
+                arr[i] -= 1
 
-print(' '.join(res))
+    if len(res) != n:
+        return "IMPOSSIBLE"
+    return ' '.join(res)
+
+
+rst = []
+for _ in range(t):
+    rst.append(test())
+
+for i in rst:
+    print(i)
