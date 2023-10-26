0e1e325069dc4c177bfc9f316183915e630b2a3f
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-26 21:48:12 +0900

    1629

--- ps.py
+++ ps.py
@@ -1,32 +1,12 @@
-# 1043
-import sys
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-t_people = set(list(map(int, input().split()))[1:])
-
-people = [[] for _ in range(n+1)]
-parties = []
-rst = []
-for _ in range(m):
-    party = set(list(map(int, input().split()))[1:])
-    parties.append(party)
-    rst.append(party)
-
-while True:
-    flag = False
-    for i in range(len(parties)):
-        if len(t_people & parties[i]) > 0:
-            t_people = t_people | parties[i]
-            parties[i] = set()
-            flag = True
-
-    if not flag:
-        break
-
-res = 0
-for party in rst:
-    if len(t_people & party) == 0:
-        res += 1
-print(res)
-
+def power(a, b, c):
+    if b == 0:
+        return 1
+    elif b%2 == 0: # 짝수인 경우
+        half_pow = power(a, b//2, c)
+        return (half_pow * half_pow) % c
+    else:
+        half_pow = power(a, (b-1) // 2, c)
+        return (a * half_pow * half_pow) % c
+    
+a, b, c = map(int, input().split())
+print(power(a, b, c))
