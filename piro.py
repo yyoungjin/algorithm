7c4455e9779e5aa79863da48b40e88a8edafd517
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-02 15:36:40 +0900

    15652

--- piro.py
+++ piro.py
@@ -1,24 +1,12 @@
-# 11054
+# 15652
 import sys
+from itertools import combinations_with_replacement
 input = sys.stdin.readline
-n = int(input())
-arr = list(map(int, input().split()))
 
-dp = [[0, 0] for _ in range(n)]
-for i in range(n):
-    long, short = 1, 1
-    for j in range(i):
-        if arr[j] < arr[i]:
-            long = max(long, dp[j][0]+1)
-        elif arr[j] > arr[i]:
-            short = max(short, max(dp[j])+1)
+n, m = map(int, input().split())
 
-    dp[i][0] = long
-    dp[i][1] = short
+arr = [x for x in range(1, n+1)]
+comb = combinations_with_replacement(arr, m)
 
-res = 0
-for i in range(n):
-    tmp = max(dp[i])
-    res = max(tmp, res)
-
-print(res)
+for c in comb:
+    print(' '.join(list(map(str, c))))
