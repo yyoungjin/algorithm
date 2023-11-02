fff08e57520f1344fa31f171246e2be3ec60c547
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-02 16:43:15 +0900

    15657

--- piro.py
+++ piro.py
@@ -1,12 +1,11 @@
-# 15652
+# 15657
 import sys
 from itertools import combinations_with_replacement
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
-arr = [x for x in range(1, n+1)]
+arr = sorted(list(map(int, input().split())))
 comb = combinations_with_replacement(arr, m)
 
-for c in comb:
+for c in sorted(comb):
     print(' '.join(list(map(str, c))))
