39acdb030442de2487a2275216bba2cd409f25b1
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-03 14:20:43 +0900

    골드4

--- piro.py
+++ piro.py
@@ -1,11 +1,27 @@
-# 15666
+# 11053
 import sys
-from itertools import combinations_with_replacement
 input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
 
-n, m = map(int, input().split())
-arr = sorted(list(map(int, input().split())))
-comb = combinations_with_replacement(arr, m)
+dp = [0] * n
+table = [[] for _ in range(n)]
 
-for c in sorted(list(set(comb))):
-    print(' '.join(list(map(str, c))))
+for i in range(n):
+    count = 1
+    tmp = [arr[i]]
+    for j in range(i):
+        if arr[j] < arr[i]:
+            if count < (dp[j]+1):
+                count = dp[j]+1
+                tmp = table[j] + [arr[i]]
+
+
+    dp[i] = count
+    table[i] = tmp
+
+max_value = max(dp)
+max_table = table[dp.index(max_value)]
+
+print(max_value)
+print(' '.join(map(str, max_table)))
