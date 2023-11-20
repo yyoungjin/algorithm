2ed1f116af0a59f7aec1fb5356de2547aff31b5b
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-21 01:02:57 +0900

    6086

--- piro.py
+++ piro.py
@@ -1,22 +1,30 @@
-# 17412
+#6086
 import sys
-input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
 from collections import deque
 
-# n개의 정점과 p개의 간선
-n, p = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+n = 60
 
-# capacity, flow
+# graph, capacity, flow
+graph = [[] for _ in range(n+1)]
 capacity = [[0] * (n+1) for _ in range(n+1)]
 flow = [[0] * (n+1) for _ in range(n+1)]
 
 # graph update u -> v
-for _ in range(p):
-    u, v = map(int, input().split())
+for _ in range(int(input())):
+    u, v, c = map(str, sys.stdin.readline().rstrip().split())
+    u, v, c = ord(u) - 64, ord(v) - 64, int(c)
+    # 이미 존재하는 간선인지
+    if capacity[u][v] > 0:
+        capacity[u][v] += c
+        capacity[v][u] += c
+        continue
+
+    # 정방향
     graph[u].append(v)
     graph[v].append(u) # 음수 유량 고려
-    capacity[u][v] = 1 # 경로 겹치면 안 되므로 1
+    capacity[u][v] = c
+    capacity[v][u] = c
 
 
 def bfs_path(source, sink, visited): # source부터 sink까지 흘릴 수 있는 추가 유량이 있는지, 있다면 그 경로를 visited에 저장
@@ -44,26 +52,25 @@ def edmonds_karp(source, sink):
         if not bfs_path(source, sink, visited): # path 가 없다면 업데이트 종료
             break
         
-        # ## min_flow 찾는 과정
-        # min_flow = 1 # 용량의 최댓값, 여기선 1
-        # j = sink
-        # while j != source:
-        #     i = visited[j]
-        #     leftover_flow = capacity[i][j] - flow[i][j]
-        #     if not leftover_flow: # 0이라면
-        #         min_flow = 0
-        #     j = i
-        
-        ## 위에서 찾은 min_flow를 flow에 업데이트
+        ## min_flow 찾는 과정
+        min_flow = 1e9
         j = sink
         while j != source:
             i = visited[j]
-            flow[i][j] += 1
-            flow[j][i] -= 1
+            leftover_flow = capacity[i][j] - flow[i][j]
+            min_flow = min(min_flow, leftover_flow)
+            j = i
+        
+        # 위에서 찾은 min_flow를 flow에 업데이트
+        j = sink
+        while j != source:
+            i = visited[j]
+            flow[i][j] += min_flow
+            flow[j][i] -= min_flow
             j = i
 
-        ans += 1
+        ans += min_flow
+
     return ans
 
-
-print(edmonds_karp(1,2))
+print(edmonds_karp(1, 26))
--- team_note.py
+++ team_note.py
@@ -70,3 +70,63 @@ def is_prime(number):
         if number % i == 0:
             return False
     return True
+
+
+#### 에드몬드카프 알고리즘
+# graph, capacity, flow
+graph = [[] for _ in range(n+1)]
+capacity = [[0] * (n+1) for _ in range(n+1)]
+flow = [[0] * (n+1) for _ in range(n+1)]
+
+# graph update u -> v
+for _ in range(p):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u) # 음수 유량 고려
+    capacity[u][v] = 
+
+def bfs_path(source, sink, visited): # source부터 sink까지 흘릴 수 있는 추가 유량이 있는지, 있다면 그 경로를 visited에 저장
+    q = deque()
+    q.append(source)
+    while q and visited[sink] == -1: # 갈 곳이 남아있고, 아직 도착점에 도달 안했다면 반복
+        u = q.popleft()
+        for v in graph[u]: # 역방향을 포함하여 연결된 다음 정점 모두 순회
+            leftover_flow = capacity[u][v] - flow[u][v]
+            if visited[v] == -1 and leftover_flow > 0: # 방문한 적 없고, 잔여용량 남아 있으면 방문
+                q.append(v)
+                visited[v] = u # 어디서 왔는지 기록
+                if v == sink: # sink에 도착했다면 종료
+                    break
+
+    if visited[sink] == -1: # 경로가 없다는 뜻
+        return False
+    
+    return True
+
+def edmonds_karp(source, sink):
+    ans = 0
+    while True: # 업데이트할 유량이 있으면 반복, 즉 bfs 결과에 따라 결정
+        visited = [-1] * (n+1)
+        if not bfs_path(source, sink, visited): # path 가 없다면 업데이트 종료
+            break
+        
+        ## min_flow 찾는 과정
+        min_flow = 1e9
+        j = sink
+        while j != source:
+            i = visited[j]
+            leftover_flow = capacity[i][j] - flow[i][j]
+            min_flow = min(min_flow, leftover_flow)
+            j = i
+        
+        # 위에서 찾은 min_flow를 flow에 업데이트
+        j = sink
+        while j != source:
+            i = visited[j]
+            flow[i][j] += min_flow
+            flow[j][i] -= min_flow
+            j = i
+
+        ans += min_flow
+
+    return ans
