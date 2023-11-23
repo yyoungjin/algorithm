492a2c7b97f1872e3ddeecd8420c6fce9702e578
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-23 15:11:11 +0900

    12015

--- piro.py
+++ piro.py
@@ -6,12 +6,24 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 dp = [arr[0]]
+table = []
 for i in range(n):
     if arr[i] > dp[-1]:
         dp.append(arr[i])
+        table.append((arr[i], len(dp)-1))
 
     else:
         idx = bisect.bisect_left(dp, arr[i])
         dp[idx] = arr[i]
+        table.append((arr[i], idx))
 
-print(len(dp))
+count = len(dp)
+print(count)
+
+res = [0] * count
+for num, idx in table[::-1]:
+    if idx == count - 1:
+        res[idx] = num
+        count -= 1
+
+print(' '.join(map(str, res)))
