bf50458208369245b97cec765b3001cdc4caa8a2
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-24 14:47:57 +0900

    20040

--- piro.py
+++ piro.py
@@ -1,56 +1,41 @@
-# 16946
+#20040
+#### 유니온파인드
+def find(x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
 import sys
 input = sys.stdin.readline
-from collections import deque
 
+# 노드의 개수와 간선(Union 연산)의 개수 입력 받기
 n, m = map(int, input().split())
-table = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
+parent = [0] * n
 
-visited = [[0] * m for _ in range(n)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y, num):
-    visited[x][y] = num
-    q = deque([(x, y)])
-
-    res = set()
-    while q:
-        x, y = q.popleft()
-        res.add((x, y))
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and table[nx][ny] == 0:
-                visited[nx][ny] = num
-                q.append((nx, ny))
-
-    return len(res)
-
-rst = {}
-walls = []
-num = 2
 for i in range(n):
-    for j in range(m):
-        if not visited[i][j] and table[i][j] == 0:
-            rst[num] = bfs(i, j, num)
-            num += 1
-        elif table[i][j] == 1:
-            walls.append((i, j))
+    parent[i] = i
 
-for i, j in walls:
-    count = 0
-    tmp = set()
-    for d in range(4):
-        ni = dx[d] + i
-        nj = dy[d] + j
-        if 0<=ni<n and 0<=nj<m and visited[ni][nj] > 1:
-            num = visited[ni][nj]
-            if num not in tmp:
-                count += rst[num]
-                tmp.add(num)
+# Union 연산을 각각 수행
+flag = 0
+for i in range(m):
+    a, b = map(int, input().split())
+    if flag:
+        continue
+    if find(a) == find(b):
+        flag = i+1
+    union(a, b)
 
-    table[i][j] = (count + 1) % 10
-
-for line in table:
-    print(''.join(map(str, line)))
+if flag:
+    print(flag)
+else:
+    print(0)
