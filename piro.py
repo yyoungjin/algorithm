24f0fb73d5f50708e02634843f38e6db7c88815f
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-24 23:41:57 +0900

    2473

--- piro.py
+++ piro.py
@@ -1,19 +1,36 @@
-a, b = map(int, input().split())
-b += 1
+import sys
+input = sys.stdin.readline
+from itertools import combinations
+import bisect
 
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
 
-def test(a):
-    t = 2**61
-    res = 0
-    for i in range(60):
-        t = t // 2
-        if a // t:
-            res += (a//t * (t//2))
-            if a % t > (t//2):
-                res += (a%t - (t//2))
-        elif t//2 < a:
-            res += (a - t//2)
-    return res
+comb = combinations([i for i in range(n)], 2)
+cnt = 1e10
+for a, b in comb:
+    l = arr[a]+arr[b]
+    tmp2 = bisect.bisect_left(arr, -l)
+    tmp1 = tmp2 - 1
 
+    while tmp1 == a or tmp1 == b:
+        tmp1 -= 1
+    while tmp2 == a or tmp2 == b:
+        tmp2 += 1
+    
+    if 0<= tmp1 < n and 0<= tmp2<n:
+        if abs(l+arr[tmp1]) < abs(l+arr[tmp2]):
+            tmp = tmp1
+        else:
+            tmp = tmp2
+    elif 0<= tmp1 < n:
+        tmp = tmp1
+    else:
+        tmp = tmp2
 
-print(test(b) - test(a))
+    if abs(l+arr[tmp]) < cnt:
+        cnt = abs(l+arr[tmp])
+        res = (arr[a], arr[b], arr[tmp])
+
+print(' '.join(map(str, res)))
