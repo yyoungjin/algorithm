d282a48e747953442a3f723fcd42b17be099b760
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-27 01:21:43 +0900

    1644

--- ps.py
+++ ps.py
@@ -1,26 +1,37 @@
-# 1806
-import sys
-input = sys.stdin.readline
+# 1644
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n+1)  # 0부터 n까지의 모든 수를 포함하는 리스트 생성, 초기값은 모두 소수로 가정
 
-n, s = map(int, input().split())
-arr = list(map(int, input().split()))
+    primes[0], primes[1] = False, False  # 0과 1은 소수가 아니므로 False로 설정
 
-INF = 1e5
+    p = 2
+    while p * p <= n:
+        if primes[p] == True:  # p가 소수인 경우
+            for i in range(p * p, n + 1, p):
+                primes[i] = False  # p의 배수들을 모두 소수가 아닌 것으로 표시
+
+        p += 1
+
+    prime_numbers = [num for num in range(n+1) if primes[num]]  # 소수들을 리스트에 저장
+    return prime_numbers
+
+n = int(input())
+arr = sieve_of_eratosthenes(n)
 
 ep = 0
+if not arr:
+    print(0)
+    exit()
 num = arr[0]
-size = INF
-for sp in range(n):
-    while num < s and ep < n-1:
+count = 0
+for sp in range(len(arr)):
+    while num < n and ep < (len(arr)-1):
         ep += 1
         num += arr[ep]
-
-    if num >= s:
-        size = min(size, (ep - sp + 1))
+    
+    if num == n:
+        count += 1
     
     num -= arr[sp]
 
-if size == INF:
-    print(0)
-else:
-    print(size)
+print(count)
