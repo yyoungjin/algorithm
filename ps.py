3af01d0e436b99de5d8685340ff94d424690ac7a
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-11-26 17:16:44 +0900

    2003

--- ps.py
+++ ps.py
@@ -1,38 +1,20 @@
-# 27651
+# 2003
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-tmp = 0
-sum_arr = []
-for i in range(n):
-    tmp += arr[i]
-    sum_arr.append(tmp)
-
-
-def part_sum(a, b):
-    if a == 0:
-        return sum_arr[b]
-    return sum_arr[b] - sum_arr[a-1]
-
-
-res = 0
-p = n-1
-ep = 2
-for mp in range(1, n-1):
-    head = sum_arr[mp-1]
-    
-    while head >= part_sum(p, n-1):
-        p -= 1
-
-    while part_sum(mp, ep-1) <= part_sum(ep, n-1):
+ep = 0
+num = arr[0]
+count = 0
+for sp in range(n):
+    while num < m and ep < n-1:
         ep += 1
+        num += arr[ep]
+    if num == m:
+        count += 1
 
-    if ep <= p and ep < n:
-        res += (p - ep + 1)
-    else: 
-        break
+    num -= arr[sp]
 
-print(res)
+print(count)
